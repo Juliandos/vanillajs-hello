434b34d6bbac2212c6b65033800bc0522d184c30
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-01-02 21:40:55 +0000

    Julian Ortega

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'A random jogger'];
+  let action = ['ate', 'kicked', 'stole', 'crushed'];
+  let what = ['my homework', 'my sandwich', 'the car', 'my phone'];
+  let when = ['before the class', 'on the street', 'during lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
+
+document.getElementById('excuse').innerText = generateExcuse();
